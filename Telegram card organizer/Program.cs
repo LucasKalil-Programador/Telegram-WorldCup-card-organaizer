0819eb567ebaf2c1fade227e74ccf86b4001b416
LucasKalil-Programador <82661706+LucasKalil-Programador@users.noreply.github.com>
2022-08-30 19:16:59 -0300

    Adicionar arquivos de projeto.

---
+++ Telegram card organizer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Telegram_card_organizer.Utils;
+
+
+
+DateTime time = DateTime.Now;
+
+string[] figures = Figures.List;
+
+
+Console.WriteLine(DateTime.Now - time);
+Console.WriteLine(Figures.List);
+Console.ReadLine();
+
+
+
+
+
+
---
+++ Telegram card organizer/Utils/Figures.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram_card_organizer.Utils
+{
+    #region Figures
+
+    public static class Figures
+    {
+        public static string[] List => GenerateFigures(); 
+
+        private static string[] GenerateFigures()
+        {
+            List<string> cards = new List<string>(678);
+
+            for (int i = 0; i <= 29; i++)
+                cards.Add($"FWC {i}");
+
+            string[] teams = { "QAT", "ECU", "SEN", "NED", "ENG", "IRN", "USA", "WAL",
+                           "ARG", "KSA", "MEX", "POL", "FRA", "AUS", "DEN", "TUN",
+                           "ESP", "CRC", "GER", "JPN", "BEL", "CAN", "MAR", "CRO",
+                           "BRA", "SRB", "SUI", "CMR", "POR", "GHA", "URU", "KOR" };
+
+            foreach (var team in teams)
+            {
+                for (int i = 1; i <= 20; i++)
+                    cards.Add($"{team} {i}");
+            }
+
+            for (int i = 1; i <= 8; i++)
+                cards.Add($"C{i}");
+
+            return cards.ToArray();
+        }
+    }
+
+    #endregion
+}
